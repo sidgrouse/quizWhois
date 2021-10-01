7380a8b7ff348bd99be06963bf838400b8b33810
Дмитрий Саранцев <sarancev.vrn@gmail.com>
2021-10-01 17:49:17 +0300

    CreateProject
    Add Question entity

---
+++ QuizWhois/Domain/Database/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Question>().HasKey(q => q.Id);
+        }
+    }
+}
---
+++ QuizWhois/Domain/Entity/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Question : Base.Base
+    {
+        public string QuestionText { get; set; }
+        public string Answer { get; set; }
+    }
+}

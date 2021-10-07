854545714a2fe62a25efa496841a7427644f7fa8
Mikhail Voronkov <misvort@gmail.com>
2021-10-07 23:35:22 +0300

    Add Stylecop Analyzer
    ruleset for every csproj, maybe one for solution is enough, but for now it's ok

--- QuizWhois.Common/Models/QuestionModel.cs
+++ QuizWhois.Common/Models/QuestionModel.cs
@@ -9,7 +9,9 @@ namespace QuizWhois.Common.Models
     public class QuestionModel
     {
         public long Id { get; set; }
+
         public string QuestionText { get; set; }
+
         public string CorrectAnswer { get; set; }
 
         public QuestionModel(long id, string questionText, string correctAnswer)
--- QuizWhois.Domain/Database/ApplicationContext.cs
+++ QuizWhois.Domain/Database/ApplicationContext.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
-using QuizWhois.Domain.Entity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuizWhois.Domain.Entity;
 
 namespace QuizWhois.Domain.Database
 {
     public class ApplicationContext : DbContext
     {
         public DbSet<Question> Questions { get; set; }
+
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
